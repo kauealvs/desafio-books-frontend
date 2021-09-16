e1babc2e5e70920c1fe937cab602a910d51198d0
kauealvs <kauealvesdesena@gmail.com>
2021-09-15 21:59:55 -0300

    initial commit structure

---
+++ src/components/LoginPage/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function LoginPage() {
+  return (
+    <section className="login-page-container-sectioon">
+      <p className=".LoginPage">Teste</p>
+    </section>
+  );
+}
+
+export default LoginPage;
